12e187e703f0cbcbdbfbfa3ecffd15c5e5190418
bob4o-afk <bobkatasad@abv.bg>
2024-11-18 07:39:56 +0000

    feat: adding budget and expenses

--- codbex-projects-reports/gen/report-model/dao/projectReport/projectReportRepository.ts
+++ codbex-projects-reports/gen/report-model/dao/projectReport/projectReportRepository.ts
@@ -2,6 +2,11 @@ import { Query, NamedQueryParameter } from "sdk/db";
 
 export interface projectReport {
     readonly 'Project': string;
+    readonly 'TotalBudget': number;
+    readonly 'TotalContingencyReserves': number;
+    readonly 'TotalExpense': number;
+    readonly 'RemainingBudgetWithoutReserves': number;
+    readonly 'RemainingBudgetWithReserves': number;
 }
 
 export interface projectReportFilter {
@@ -23,10 +28,27 @@ export class projectReportRepository {
 
     public findAll(filter: projectReportPaginatedFilter): projectReport[] {
         const sql = `
-            SELECT codbexProject.PROJECT_NAME as "Project"
+            SELECT codbexProject.PROJECT_NAME as "Project", 
+                   COALESCE(B.TotalBudget, 0) as "TotalBudget",
+                   COALESCE(B.TotalContingencyReserves, 0) as "TotalContingencyReserves",
+                   COALESCE(E.TotalExpense, 0) as "TotalExpense",
+                   (COALESCE(B.TotalBudget, 0) - COALESCE(B.TotalContingencyReserves, 0) - COALESCE(E.TotalExpense, 0)) as "RemainingBudgetWithoutReserves",
+                   (COALESCE(B.TotalBudget, 0) - COALESCE(E.TotalExpense, 0)) as "RemainingBudgetWithReserves"
             FROM CODBEX_PROJECT as codbexProject
+            LEFT JOIN (
+                SELECT BUDGET_PROJECT, 
+                       SUM(BUDGET_AMOUNT) AS TotalBudget, 
+                       SUM(BUDGET_CONTINGENCYRESERVES) AS TotalContingencyReserves
+                FROM CODBEX_BUDGET
+                GROUP BY BUDGET_PROJECT
+            ) B ON B.BUDGET_PROJECT = codbexProject.PROJECT_ID
+            LEFT JOIN (
+                SELECT EXPENSE_PROJECT, SUM(EXPENSE_AMOUNT) AS TotalExpense
+                FROM CODBEX_EXPENSE
+                GROUP BY EXPENSE_PROJECT
+            ) E ON E.EXPENSE_PROJECT = codbexProject.PROJECT_ID
             WHERE codbexProject.PROJECT_NAME LIKE :PROJECT_NAME
-            ORDER BY PROJECT_NAME ASC
+            ORDER BY codbexProject.PROJECT_NAME ASC
             ${Number.isInteger(filter.$limit) ? ` LIMIT ${filter.$limit}` : ''}
             ${Number.isInteger(filter.$offset) ? ` OFFSET ${filter.$offset}` : ''}
         `;
@@ -44,10 +66,27 @@ export class projectReportRepository {
     public count(filter: projectReportFilter): number {
         const sql = `
             SELECT COUNT(*) as REPORT_COUNT FROM (
-                SELECT codbexProject.PROJECT_NAME as "Project"
+                SELECT codbexProject.PROJECT_NAME as "Project", 
+                       COALESCE(B.TotalBudget, 0) as "TotalBudget",
+                       COALESCE(B.TotalContingencyReserves, 0) as "TotalContingencyReserves",
+                       COALESCE(E.TotalExpense, 0) as "TotalExpense",
+                       (COALESCE(B.TotalBudget, 0) - COALESCE(B.TotalContingencyReserves, 0) - COALESCE(E.TotalExpense, 0)) as "RemainingBudgetWithoutReserves",
+                       (COALESCE(B.TotalBudget, 0) - COALESCE(E.TotalExpense, 0)) as "RemainingBudgetWithReserves"
                 FROM CODBEX_PROJECT as codbexProject
+                LEFT JOIN (
+                    SELECT BUDGET_PROJECT, 
+                           SUM(BUDGET_AMOUNT) AS TotalBudget, 
+                           SUM(BUDGET_CONTINGENCYRESERVES) AS TotalContingencyReserves
+                    FROM CODBEX_BUDGET
+                    GROUP BY BUDGET_PROJECT
+                ) B ON B.BUDGET_PROJECT = codbexProject.PROJECT_ID
+                LEFT JOIN (
+                    SELECT EXPENSE_PROJECT, SUM(EXPENSE_AMOUNT) AS TotalExpense
+                    FROM CODBEX_EXPENSE
+                    GROUP BY EXPENSE_PROJECT
+                ) E ON E.EXPENSE_PROJECT = codbexProject.PROJECT_ID
                 WHERE codbexProject.PROJECT_NAME LIKE :PROJECT_NAME
-                ORDER BY PROJECT_NAME ASC
+                ORDER BY codbexProject.PROJECT_NAME ASC
             )
         `;
 
